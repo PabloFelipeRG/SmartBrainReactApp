9d76b2568edb4e6bc3822ee533b9b46ee395717d
Pablo Goncalves <pablo.feliperios@gmail.com>
2018-11-11 17:25:06 -0200

    commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav style={{display: 'flex', justifyContent:'flex-end'}}>
+            <p className='f3 fw6 db white link dim pointer'>Sign Out</p>
+        </nav>
+    );
+}
+
+export default Navigation;
