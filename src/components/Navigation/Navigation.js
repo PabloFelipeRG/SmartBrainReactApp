695ee294d514ba2f70cbb31384efca8fa19b3d9e
Pablo Goncalves <pablo.feliperios@gmail.com>
2018-11-11 17:41:53 -0200

    tilt and logo

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () => {
     return (
         <nav style={{display: 'flex', justifyContent:'flex-end'}}>
-            <p className='f3 fw6 db white link dim pointer'>Sign Out</p>
+            <p className='f3 fw6 db white link dim pointer pa3'>Sign Out</p>
         </nav>
     );
 }
