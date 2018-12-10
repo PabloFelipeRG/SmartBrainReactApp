76341e24d16661b29b1c4f741bf6aa2de2347101
Pablo Goncalves <pablo.goncalves@discover.com.br>
2018-12-10 17:07:03 -0200

    API added + Signin form.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
 import Logo from './components/Logo/Logo';
 import Particles from 'react-particles-js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: '4f4be64305674a3cb76ec2ebd4b08344'
+});
+//http://revistasaudeeestilo.com.br/wp-content/uploads/2018/04/harmoniafacial-1.jpg
 const particlesOptions = {
   "particles": {
     "number": {
@@ -71,6 +78,52 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(clarifaiFace);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+    console.log(this.state.box);
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input
+    ).then(response => {
+      this.displayFaceBox(this.calculateFaceLocation(response));
+    }).catch(err => {
+      console.log("YOU GOT AN ERROR:", err);
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -79,10 +132,17 @@ class App extends Component {
           params={particlesOptions}
         />
         <Navigation />
+        <Signin />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition
+          box={this.state.box}
+          imageUrl={this.state.imageUrl}
+        />
       </div>
     );
   }
